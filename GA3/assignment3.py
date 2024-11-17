658298265f81e22f1a910c350c220003e154e932
Ellie <dinge@oregonstate.edu>
2024-11-16 17:30:33 -0800

    didn't work :(

--- GA3/assignment3.py
+++ GA3/assignment3.py
@@ -9,45 +9,49 @@ class Edge:
 class UnionFind:
     def __init__(self, num_vertices):
         self.parent = list(range(num_vertices))
+        self.rank = [0] * num_vertices
 
-def check_cycle(adj_list, s, new_edge):
-    new_adj_list = adj_list.copy()
-    new_adj_list[new_edge.node].append(new_edge)
-
-    visited = set()
-    queue = deque([(s, None)])
-
-    visited.add(s)
-    while (queue):
-        #print(visited, queue)
-
-        v, parent = queue.popleft()
-
-        for neighbor in new_adj_list[v]:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.append((neighbor, v))
-            elif neighbor != parent:
-                #print(neighbor, parent)
-                return True
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
     
-    return False
+    def union(self, i, j):
+        i_root = self.find(i)
+        j_root = self.find(j)
+
+        if self.rank[i_root] > self.rank[j_root]:
+            self.parent[j_root] = i
+        elif self.rank[i_root] < self.rank[j_root]:
+            self.parent[i_root] = j_root
+        else:
+            self.parent[j_root] = i_root
+            self.rank[i_root] += 1
         
-def first_min_spanning_tree(edges):
-    adj_list = defaultdict(list)
-    min_spanning_tree = set()
-    for edge in edges:
+def base_min_spanning_tree(edges, num_vertices, min_spanning_tree, uf):
+    index_of_last_edge_added = 0
+
+    for n in range(num_vertices-2):
         # check = check_cycle(adj_list, edge.node)
         # print(adj_list)
-        if (not check_cycle(adj_list, edge.node, edge) and not check_cycle(adj_list, edge.neighbor, edge)):
-            min_spanning_tree.add(edge)
-            adj_list[edge.node].append(edge)
+        
+        i = uf.find(edges[n].node)
+        j = uf.find(edges[n].neighbor)
+        if i != j:
+            min_spanning_tree.append(edges[n])
+            index_of_last_edge_added = n
+            uf.union(i, j)
     
-    sum = 0
-    for edge in min_spanning_tree:
-        sum += edge.weight
-    
-    return sum
+    return index_of_last_edge_added
+
+def find_min_spanning_tree(edges, index, uf):
+    while index < len(edges):
+        i = uf.find(edges[index].node)
+        j = uf.find(edges[index].neighbor)
+        if i != j:
+            return index
+        else:
+            index += 1 
 
 def three_min_spanning_trees(input_file_path, output_file_path):
     with open(input_file_path, 'r') as input_file:
@@ -61,22 +65,28 @@ def three_min_spanning_trees(input_file_path, output_file_path):
             adj_matrix[i] = [int(num) for num in line]
     
     # Turns adjacency matrix into array of edge objects
-    edges = set()
+    edges = []
     for i in range(0, num_vertices-1):
         for j in range(i+1, num_vertices):         
             if adj_matrix[i][j] != 0:
-                edges.add(Edge(i, j, adj_matrix[i][j]))
-    
-    
-    for edge in edges:
-        print(edge.node, edge.neighbor)
+                edges.append(Edge(i, j, adj_matrix[i][j]))
     
     # Sorts the edges
     edges = sorted(edges, key=lambda edge: edge.weight)
 
-    first = first_min_spanning_tree(edges)
+    min_spanning_tree = []
+    uf = UnionFind(num_vertices)
 
-    print(first)
+    for node in range(num_vertices):
+        uf.parent.append(node)
+        uf.rank.append(0)
+    index = base_min_spanning_tree(edges, num_vertices, min_spanning_tree, uf)
+
+    sum = 0
+    for edge in min_spanning_tree:
+        sum += edge.weight
+
+    print(sum)
     
     # with open(output_file_path, 'w') as output_file:
     #     output_file.write(str())
